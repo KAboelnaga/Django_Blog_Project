37b9bcc8ccca8867d8562f5884cf03839154b515
bothaina hassan <58982280+bothaina1@users.noreply.github.com>
2025-07-14 00:58:05 +0300

    making the comment model and initial view

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    ID=models.AutoField(primary_key=True,editable=False)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
