ba73c554ca2dff78d208c409874fbb6b2e1b378b
blaack007 <mohamed.khalifa4053@gmail.com>
2025-07-16 14:40:51 +0300

    feat: Add email configuration
    - Created a new .env file for email host configuration.
    - Updated .gitignore to remove the .env exclusion.
    - Enhanced the context processor to include all categories in the user subscriptions context.
    - Updated templates to improve the display of categories and subscription buttons.

--- subscriptions/context_processors.py
+++ subscriptions/context_processors.py
@@ -1,7 +1,9 @@
+from blogs.models import Category
 from .models import Subscription
 
 def user_subscriptions(request):
     if request.user.is_authenticated:
         subscriptions = Subscription.objects.filter(user=request.user).values_list('category_id', flat=True)
-        return {'user_subscriptions': list(subscriptions)}
-    return {'user_subscriptions': []} 
+        categories = Category.objects.all()
+        return {'user_subscriptions': list(subscriptions), 'categories': categories}
+    return {'user_subscriptions': [], 'categories': Category.objects.all()} 
